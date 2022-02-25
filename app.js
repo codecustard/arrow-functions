657051567ff102ae69e610c01f54774ccf01e6bb
codecustard <emmanuelbarroga@gmail.com>
2022-02-25 12:32:52 -0800

    step 11. difference of arrow function with this

--- app.js
+++ app.js
@@ -190,17 +190,17 @@ Student.prototype.scope = function () {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+//    'this' is the joe object
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+//    'this' is the window object
 // 3. Explain why "this" is different when an arrow function is used.
-//
+//   arrow functions don't have a 'this' value, so it's the window object.
